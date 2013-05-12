760a7bd283858888e19cc4c1aba07bfe345ff576
Dmitry Korotovsky <rmpic30@gmail.com>
2013-05-12 18:23:41 +0400

    New tests

--- src/test/main/com/korotovsky/pusherd/GameServerTest.java
+++ src/test/main/com/korotovsky/pusherd/GameServerTest.java
@@ -1,17 +1,69 @@
 package com.korotovsky.pusherd;
 
+import com.korotovsky.pusherd.client.Player;
+import com.korotovsky.pusherd.core.Game;
+import com.korotovsky.pusherd.core.PlayerGame;
+import com.korotovsky.pusherd.network.ClientSocket;
+
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
 import java.util.logging.Logger;
 
 public class GameServerTest {
+    private static GameServer gameServer;
+
+    @BeforeClass
+    public static void initServer() {
+        gameServer = new GameServer(Logger.getLogger("pusherd"));
+    }
 
     @Test
     public void testGetLogger() {
-        Logger logger = Logger.getLogger("pusherd");
-        GameServer gameServer = new GameServer(logger);
+        Assert.assertThat(gameServer.getLogger(), CoreMatchers.instanceOf(Logger.class));
+    }
 
-        Assert.assertEquals(logger, gameServer.getLogger());
+    @Test
+    public void testGetGame() {
+        Assert.assertThat(gameServer.getGame(), CoreMatchers.instanceOf(Game.class));
+    }
+
+    @Test
+    public void testGetPlayers() {
+        Assert.assertThat(gameServer.getPlayers(), CoreMatchers.instanceOf(ConcurrentHashMap.class));
+    }
+
+    @Test
+    public void testGetWorkers() {
+        Assert.assertThat(gameServer.getWorkers(), CoreMatchers.instanceOf(ExecutorService.class));
+    }
+
+    @Test
+    public void testIsAlive() {
+        Assert.assertEquals(true, gameServer.isAlive());
+    }
+
+    @Test
+    public void testGetPlayersCount() {
+        Assert.assertEquals(new Integer(0), gameServer.getPlayersCount());
+    }
+
+    @Test
+    public void testOnPutClient() {
+        gameServer.onPutClient(new ClientSocket(gameServer, new Socket()), new Player(new PlayerGame()));
+
+        Assert.assertEquals(new Integer(1), gameServer.getPlayersCount());
+    }
+
+    @Test
+    public void testOnRemoveClient() {
+        gameServer.onRemoveClient(new ClientSocket(gameServer, new Socket()));
+
+        Assert.assertEquals(new Integer(1), gameServer.getPlayersCount());
     }
 }
